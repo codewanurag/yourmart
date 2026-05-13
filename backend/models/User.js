418c6292a900fa8880bbac935583ecaee662227d
Rituraj-digarse <amanpawar0409@gmail.com>
2026-05-13 13:37:56 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -48,3 +48,40 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 module.exports = mongoose.model('User', userSchema);
+
+
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
